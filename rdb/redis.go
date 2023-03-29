840262d472f5ea2f8610cd20694f8c4c20a41c57
xiaozhaofu <xiaozhaofu@163.com>
2023-03-29 19:36:37 +0800

    add hash

--- rdb/redis.go
+++ rdb/redis.go
@@ -232,6 +232,50 @@ func (r *Redisclient) Llen(k string) (int64, error) {
 	return rs, nil
 }
 
+func (r *Redisclient) Hexists(key, field string) (bool, error) {
+	rs, err := r.client.HExists(r.context, key, field).Result()
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			return false, nil
+		}
+		return false, err
+	}
+	return rs, nil
+}
+
+func (r *Redisclient) Hincrby(key, field string, incr int64) (int64, error) {
+	rs, err := r.client.HIncrBy(r.context, key, field, incr).Result()
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			return 0, nil
+		}
+		return 0, err
+	}
+	return rs, nil
+}
+
+func (r *Redisclient) Hset(key string, val ...any) (int64, error) {
+	rs, err := r.client.HSet(r.context, key, val).Result()
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			return 0, nil
+		}
+		return 0, err
+	}
+	return rs, nil
+}
+
+func (r *Redisclient) Hget(key, field string) (string, error) {
+	rs, err := r.client.HGet(r.context, key, field).Result()
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			return "", nil
+		}
+		return "", err
+	}
+	return rs, nil
+}
+
 // Select 选择指定的 db
 func (r *Redisclient) Select(db int) {
 	_, err := r.client.Pipelined(r.context, func(pipeliner redis.Pipeliner) error {
